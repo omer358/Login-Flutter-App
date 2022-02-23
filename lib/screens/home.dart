73151408babe73ce00a553c7d847e928e8edb538
omer358 <omermaki358@gmail.com>
2022-02-23 10:28:42 +0200

    Add BottmNavigationBar to the home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,6 +9,27 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Second Route'),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_alt),
+            label: 'Friends',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+        ],
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
