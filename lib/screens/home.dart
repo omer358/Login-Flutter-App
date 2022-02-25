deed45d50e6b552fd51d75c256affd41e541a865
omer358 <omermaki358@gmail.com>
2022-02-25 10:34:12 +0200

    add basic drawer

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -39,15 +39,45 @@ class _HomeState extends State<Home> {
             backgroundColor: Colors.white,
             child: IconButton(
                 color: Colors.black,
-                padding: const EdgeInsets.all(8),
-                iconSize: 20,
-                icon: const Icon(Icons.person),
+                padding: const EdgeInsets.all(6),
+                iconSize: 30,
+                icon: Image.asset('assets/images/login.png'),
                 onPressed: () {
                   // do something
                 }),
           ),
         ],
         title: const Text('Second Route'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.teal),
+              child: CircleAvatar(
+                backgroundColor: Colors.black,
+                radius: 120,
+                child: CircleAvatar(
+                  radius: 110,
+                  backgroundImage: AssetImage('assets/images/login.png'),
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text("profile"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Settings"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Logout"),
+              onTap: () {},
+            )
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         elevation: 8.0,
